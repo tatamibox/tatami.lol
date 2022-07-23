b22d3a8e22a6656d8ad19b383eae531801a08b67
tatamibox <tat4mi@gmail.com>
2022-07-23 12:16:13 -0700

    create dynamic background colors for badges

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,11 +1,15 @@
-import { useEffect, useState, React } from 'react'
+import { useEffect, useState, React, useRef } from 'react'
 import styles from '../styles/UserCard.module.css'
 import axios from 'axios'
+import changeBadgeColor from '../util/badge-color'
+
 function UserCard({ user }) {
 
     // fetches user rank on user load & holds league info (current user rank, etc) and 
     const [leagueInfo, setLeagueInfo] = useState()
     const [counter, setCounter] = useState(0)
+    const [badgeColor, setBadgeColor] = useState('#edf2f4')
+
     useEffect(() => {
         if (user) {
             axios.post('http://localhost:3001/getRankedData', { id: user.id })
@@ -20,15 +24,25 @@ function UserCard({ user }) {
 
     // uses the counter state, checks if the returned leagueInfo is undefined.
     // if undefined, sets leagueInfo to unranked
+    let ssString = '';
     useEffect(() => {
         if (leagueInfo === undefined) {
             setLeagueInfo({
                 tier: 'Unranked',
-                rank: ''
+                rank: '',
             })
+
         }
+        else if (leagueInfo !== undefined) {
+            //uses badge color change from util to change rank backgrounds
+            changeBadgeColor(leagueInfo.tier, setBadgeColor)
+        }
+
     }, [counter])
+
+
     //
+    console.log(leagueInfo)
 
     return (
         <>
@@ -37,7 +51,7 @@ function UserCard({ user }) {
                 <div className={styles.summonerInfo}>
                     <h2>{user.name}</h2>
                     <h3>Level {user.summonerLevel}</h3>
-                    <h3>{`${leagueInfo.tier} ${leagueInfo.rank}`}</h3>
+                    <h3 style={{ backgroundColor: `${badgeColor}` }} className={styles.summoner__rank}>{`${leagueInfo.tier} ${leagueInfo.rank}`}</h3>
                 </div>
             </section> : <div></div>}
         </>
